92dc136c7b20b671d94c9f5f371902d793004d18
Laura Leal <lleal@adaptivesecurity.cl>
2023-03-06 00:06:01 -0300

    mejora db

--- webapp/analyzer/otrs/models/db.py
+++ webapp/analyzer/otrs/models/db.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.pool import NullPool
 
 engine = create_engine(
-    #"mysql://datastudio:GYR4nka2nqt*rkv.xpg@172.16.33.8/otrs?charset=utf8mb4",
     "mariadb+mariadbconnector://datastudio:GYR4nka2nqt*rkv.xpg@172.16.33.8/otrs?charset=utf8mb4",
-    pool_size=100, pool_recycle=280
+    poolclass=NullPool
 )
+
 Session = scoped_session(sessionmaker(bind=engine))
-# Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
